bbe31575c466ee1eaaf0c41aa30fe1fd8fa9f8cd
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-02-23 15:36:29 -0600

    changed function names and variable names

--- script.js
+++ script.js
@@ -1,65 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var charLength;
+var userLengthValue;
 var upperCase;
 var lowerCase;
-var numbered;
-var sym;
-
+var number;
+var symbol;
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function() {
-    var length= prompt("Choose how many characters you want between 8 and 128.");
+
+
+function generatePassword() {
+    
+    var userLength = prompt("Choose how many characters you want between 8 and 128.");
     var upper = confirm("Do you want uppercase letters?");
     var lower = confirm("Do you want lowercase letters?");
     var numbers = confirm("Do you want numbers?");
     var symbols = confirm("Do you want symbols?");
+    
+    
+    if (userLength >= 8 && userLength <= 128) {
+        userLengthValue = parseInt(userLength);
+    } else {
+      alert("Not enough characters.");
+    }
 
+    var newPassword = [];
+    
+    // var upper = confirm("Do you want uppercase letters?");
+    if (upper===true) {
+        upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        for (i = 0; i < upperCase.length; i++) {
+            newPassword.push(upperCase[i]);
+        }
+    } 
 
-// password length
-   if (length >= 8 && length <= 128) {
-       charLength = parseInt(length);
-   }
-//uppercase letters
-  if (upper===true) {
-    upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  }
-// lowercase letters
-  if (lower===true) {
-    lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  }
-//numbers 
-  if (numbers===true) {
-   numbered = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
- }
-//special characters 
-  if (symbols===true) {
-    sym = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "'", "<", ">", "?", "/", "`", "~"];
-  }
+    // var lower = confirm("Do you want lowercase letters?");
+    if (lower===true) {
+        lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        for (i = 0; i < lowerCase.length; i++) {
+            newPassword.push(lowerCase[i]);
+    } 
+}
+
+    // var numbers = confirm("Do you want numbers?");
+    if (numbers===true) {
+        number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        for (i = 0; i < number.length; i++) {
+            newPassword.push(number[i]);
+    }
+} 
+
+    // var symbols = confirm("Do you want symbols?");
+    if (symbols===true) {
+        symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "'", "<", ">", "?", "/", "`", "~"];
+        for (i = 0; i < symbol.length; i++) {
+            newPassword.push(symbol[i]);
+    }
+}
+
+    var pass = ""
+
+        for (var i = 0; i < userLengthValue; i++) {
+            pass += newPassword[Math.floor(Math.random() * newPassword.length)];
+        }
+
+        return pass;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", () => {
+    writePassword();
 });
-
-writePassword();
-
-
- console.log(charLength);
- console.log(upperCase);
- console.log(lowerCase);
- console.log(numbered);
- console.log(sym);
- console.log(generatePassword());
-
-
-
-
-
-
-
